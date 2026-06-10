50938df6e040b83a680aa4a27ced683b31023c12
uyiedo <36536308+uyiedo@users.noreply.github.com>
2026-06-10 10:30:35 -0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#000" }}>
+      <Text style={{ color: "#fff", fontSize: 20 }}>
+        🎧 Sonorik App is Working!
+      </Text>
+    </View>
+  );
+}
