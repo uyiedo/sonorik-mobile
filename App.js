9f5a71295eacb027492bc3313bf77139e84cc340
uyiedo <36536308+uyiedo@users.noreply.github.com>
2026-06-11 09:09:58 -0100

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#000" }}>
-      <Text style={{ color: "#fff", fontSize: 20 }}>
-        🎧 Sonorik App is Working!
+    <View style={{ flex:1, justifyContent:"center", alignItems:"center", backgroundColor:"black" }}>
+      <Text style={{ color:"white", fontSize:20 }}>
+        🎧 Sonorik is working!
       </Text>
     </View>
   );
